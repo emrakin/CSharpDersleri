9bfb0408ca2535b84510c8a15a242828637c4c75
Emrah AKIN <eakin@uzmansistem.com.tr>
2020-03-12 10:15:52 +0300

    Veri Tipleri: Short Veri Tipi

---
+++ CSharpDersleri.ShortVeriTipi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpDersleri.ShortVeriTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short ilknumara = 10;
+            short ikincinumara = -15;
+
+            short maxdeger = 32767;
+            short mindeger = -32768;
+
+            Console.WriteLine("İlk Sayı: {0}", ilknumara);
+            Console.WriteLine("İlk Sayı: {0}", ikincinumara);
+
+            Console.WriteLine("Max Sayı: {0}", maxdeger);
+            Console.WriteLine("Min Sayı: {0}", mindeger);
+
+            Console.ReadLine();
+        }
+    }
+}
