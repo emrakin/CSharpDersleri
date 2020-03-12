970938d0debcf99b49bb02f6a8912cd3dcd4eb57
Emrah AKIN <eakin@uzmansistem.com.tr>
2020-03-12 12:19:26 +0300

    Veri Tipleri: Bool Veri Tipi

---
+++ CSharpDersleri.BoolVeriTipi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpDersleri.BoolVeriTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi = 8;
+
+            // sayi değişkenine atadığımız değerin 12 den büyük mü olduğunu kontrol edelim
+            bool sonuc = sayi > 12;
+            bool sonuc2 = sayi < 12;
+
+            Console.WriteLine(sonuc);
+            Console.WriteLine(sonuc2);
+            Console.ReadLine();
+        }
+    }
+}
