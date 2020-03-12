c6dc1df48f1954caab11e4175f9d5bec0863322a
Emrah AKIN <eakin@uzmansistem.com.tr>
2020-03-12 14:38:52 +0300

    Veri Tipleri: Char Veri Tipi

--- CSharpDersleri.BoolVeriTipi/Program.cs
+++ CSharpDersleri.BoolVeriTipi/Program.cs
@@ -11,10 +11,8 @@ namespace CSharpDersleri.BoolVeriTipi
 
             // sayi değişkenine atadığımız değerin 12 den büyük mü olduğunu kontrol edelim
             bool sonuc = sayi > 12;
-            bool sonuc2 = sayi < 12;
 
             Console.WriteLine(sonuc);
-            Console.WriteLine(sonuc2);
             Console.ReadLine();
         }
     }
---
+++ CSharpDersleri.CharVeriTipi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpDersleri.CharVeriTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Char veri tiplerine doğrudan karakter atayabilirsiniz.
+            char deger1 = 'A';
+
+            // Char veri tipine hexadecimal karakter karşılında değer atabilirsiniz.
+            char deger2 ='\x0058';
+
+            //Char veri tipine ASCII sayısal karşılığından Char tipine dönüşüm yapabilirsiniz.
+            char deger3 = (char)55;
+
+            //Char veri tipine Unicode karakter ataması yapabilirsiniz.
+            char deger4 = '\u0058';
+
+            Console.WriteLine(deger1);
+            Console.WriteLine(deger2);
+            Console.WriteLine(deger3);
+            Console.WriteLine(deger4);
+
+            Console.ReadLine();
+        }
+    }
+}
