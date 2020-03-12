21051dfabf8284956cacf24f76e33cfae7620353
Emrah AKIN <eakin@uzmansistem.com.tr>
2020-03-12 11:40:26 +0300

    Veri Tipleri: Byte Veri Tipi

---
+++ CSharpDersleri.ByteVeriTipi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpDersleri.ByteVeriTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte birinci = 5;
+            byte ikinci = 8;
+
+            byte maxdeger = 255;
+            byte mindeger = 0;
+
+
+            Console.WriteLine("İlk Sayı: {0}", birinci);
+            Console.WriteLine("İlk Sayı: {0}", ikinci);
+
+            Console.WriteLine("Max Sayı: {0}", maxdeger);
+            Console.WriteLine("Min Sayı: {0}", mindeger);
+
+            Console.ReadLine();
+        }
+    }
+}
