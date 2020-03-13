26d0464798d8a616d4d012a77db918bb5b8d50fc
Emrah AKIN <eakin@uzmansistem.com.tr>
2020-03-13 14:34:23 +0300

    Veri Tipleri: Decimal Veri Tipi

---
+++ CSharpDersleri.DecimalVeriTipi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpDersleri.DecimalVeriTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal sayi1 = 10;
+            decimal sayi2 = 10.5m;
+            decimal sayi3 = -10;
+            decimal sayi4 = -10.5m;
+
+            decimal maxDecimalDegeri = decimal.MaxValue;
+            decimal minDecimalDegeri = decimal.MinValue;
+
+            Console.WriteLine("Birinci sayı: {0}", sayi1);
+            Console.WriteLine("Ikinci sayı: {0}", sayi2);
+            Console.WriteLine("Üçüncü sayı: {0}", sayi3);
+            Console.WriteLine("Dördüncü sayı: {0}", sayi4);
+            Console.WriteLine("Maksimum deger: {0}", maxDecimalDegeri);
+            Console.WriteLine("Minimum deger: {0}", minDecimalDegeri);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpDersleri.DoubleVeriTipi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpDersleri.DoubleVeriTipi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deger1 = 243.14;
+            double deger2 = -1432;
+            double maxDoubleDegeri = double.MaxValue;
+            double minDoubleDegeri = double.MinValue;
+
+            Console.WriteLine("Birinci degisken: {0}",deger1);
+            Console.WriteLine("Ikinci degisken: {0}",deger2);
+            Console.WriteLine("Maksimum deger: {0}",maxDoubleDegeri);
+            Console.WriteLine("Minimum deger: {0}",minDoubleDegeri);
+
+            Console.ReadLine();
+        }
+    }
+}
